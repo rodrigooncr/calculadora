77f068ef0f0d32842cbc6a89ce396f2e7f8021b4
rodrigooncr <rodrigoncr@gmail.com>
2023-06-22 19:10:23 -0600

    calculadora_v1

---
+++ main.js
@@ -0,0 +1,53 @@
+const pantalla = document.querySelector('.pantalla');
+const botones = document.querySelectorAll('.btn');
+
+
+
+
+
+
+
+botones.forEach(boton => {
+    boton.addEventListener ('click', () =>{
+        const botonApretado = boton.textContent;
+        if(boton.id === 'c'){
+            pantalla.textContent = '0';
+            return;
+        }
+
+        if(boton.id === 'borrar'){
+            if(pantalla.textContent.length === 1 || pantalla.textContent === 'Error' ){
+                pantalla.textContent = '0' ;
+                return;
+            }else {   
+
+                pantalla.textContent = pantalla.textContent.slice(0,-1);
+                return;  
+            }
+            
+
+          
+        }
+
+
+        if(boton.id === 'igual'){
+
+            try{
+            /* eval nos decvuleve el resultado de una string que tenga simbolos matematicos */
+            pantalla.textContent = eval(pantalla.textContent);
+            }catch {
+
+                pantalla.textContent = 'Error';
+            }
+            
+            return;
+        }
+
+        if(pantalla.textContent === '0' || pantalla.textContent === 'Error'){
+            pantalla.textContent = botonApretado; 
+        }else{
+            pantalla.textContent += botonApretado;
+        }
+
+    });
+});
